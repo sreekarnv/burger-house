9b1bf2f220dd4f01250215654af16653c9fc3451
Sreekar Venkata Nutulapati <sreekarnv1@gmail.com>
2020-08-01 12:19:01 +0530

    Orders file updated

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.52ad525b6de4426a7a27eeb006aac206.js"
+  "/precache-manifest.004e1e46d4a22b7aab9fe744294a2db6.js"
 );
 
 self.addEventListener('message', (event) => {
