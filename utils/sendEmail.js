fea8ec1f1fac2834ccdd2308b6e9a3d33911307d
unknown <sreekarnv1@gmail.com>
2020-12-14 18:28:08 +0530

    dahgjas

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -7,13 +7,6 @@ const sendEmail = async (options) => {
 			user: process.env.EMAIL_USERNAME,
 			pass: process.env.EMAIL_PASSWORD,
 		},
-
-		// host: process.env.EMAIL_HOST,
-		// port: process.env.EMAIL_PORT,
-		// auth: {
-		// 	user: process.env.EMAIL_USERNAME,
-		// 	pass: process.env.EMAIL_PASSWORD,
-		// },
 	});
 
 	const mailOptions = {
