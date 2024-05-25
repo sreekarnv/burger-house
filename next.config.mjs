9876b751fd3c193aeac79d2cfacb5498ee37d5bf
Sreekar Nutulapati <sreekarnv1@gmail.com>
2024-05-25 18:01:14 +0530

    update deps for eslint and browserslist

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import("next").NextConfig} */
 const config = {
-	reactStrictMode: true,
-	swcMinify: true,
-	i18n: {
-		locales: ['en'],
-		defaultLocale: 'en',
-	},
-	images: {
-		domains: ['res.cloudinary.com'],
-	},
+  reactStrictMode: true,
+  swcMinify: true,
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
 };
 
 export default config;
